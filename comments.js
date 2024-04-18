03516cfc16fbab5dca31b50e70af5f328a3d79d2
Gabriel Alves Toyo <97889741+GGabrielToyo@users.noreply.github.com>
2024-04-18 17:12:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,69 +1,33 @@
-//crie um servidor web
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
 
-//crie um servidor web
-http.createServer((req, res) => {
-    //leia o arquivo comments.json
-    fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
+// 2. Create server
+http.createServer(function (req, res) {
+    // Parse the request containing file name
+    var pathname = url.parse(req.url).pathname;
+    // Print the name of the file for which request is made.
+    console.log("Request for " + pathname + " received.");
+
+    // Read the requested file content from file system
+    fs.readFile(pathname.substr(1), function (err, data) {
         if (err) {
             console.error(err);
-            res.writeHead(500, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'Internal server error' }));
-            return;
+            // HTTP Status: 404 : NOT FOUND
+            // Content Type: text/plain
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+        } else {
+            // Page found
+            // HTTP Status: 200 : OK
+            // Content Type: text/plain
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+
+            // Write the content of the file to response body
+            res.write(data.toString());
         }
 
-        //escreva o conteúdo do arquivo na resposta
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(data);
+        // Send the response body
+        res.end();
     });
-}).listen(3000, () => {
-    console.log('Server is listening on port 3000');
-});
-
-//execute o servidor e acesse http://localhost:3000 no navegador
-
-// Path: index.js
-//crie um servidor web
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
-//crie um servidor web
-http.createServer((req, res) => {
-    //leia o arquivo index.html
-    fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.writeHead(500, { 'Content-Type': 'text/html' });
-            res.end('<h1>Internal server error</h1>');
-            return;
-        }
-
-        //escreva o conteúdo do arquivo na resposta
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-    });
-}).listen(3000, () => {
-    console.log('Server is listening on port 3000');
-});
-
-//execute o servidor e acesse http://localhost:3000 no navegador
-
-// Path: index.html
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Comments</title>
-</head>
-
-<body>
-    <h1>Comments</h1>
-    <ul id="comments"></ul>
-    <script src="index.js"></script>
-</
+}).listen(8080); // the server is listening on port 8080
